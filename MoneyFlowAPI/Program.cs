a6e4a56b7ac379fc2a14405837714f425d22bdff
Victor Teixeira <victorteixeirx@gmail.com>
2025-10-29 12:39:51 -0300

    ajuste dominio producao

--- MoneyFlowAPI/Program.cs
+++ MoneyFlowAPI/Program.cs
@@ -45,7 +45,7 @@ builder.Services.AddCors(options =>
         policy.WithOrigins(
             "http://127.0.0.1:5500",
             "http://localhost:5500",
-            "https://moneyflowoficial.somee.com" // Produção
+            "http://moneyflowweb.somee.com/" // Produção
         )
         .AllowAnyHeader()
         .AllowAnyMethod();
